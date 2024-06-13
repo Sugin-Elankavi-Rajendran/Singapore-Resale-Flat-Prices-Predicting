ba9c2cd9f4e0475c2d4e1d70df6d900ea3d46211
Sugin Elankavi Rajendran <57343300+Sugin-Elankavi-Rajendran@users.noreply.github.com>
2024-06-13 15:26:26 +0530

    Finding missing values

---
+++ Data Pre-processing.py
@@ -0,0 +1,14 @@
+import glob
+import pandas as pd
+import json
+import requests
+
+df = pd.concat([pd.read_csv(f) for f in glob.glob("./data/*.csv")], ignore_index=True)
+#print(df.head())
+
+#print(df.shape)
+
+print(df.isnull().sum())
+
+
+
