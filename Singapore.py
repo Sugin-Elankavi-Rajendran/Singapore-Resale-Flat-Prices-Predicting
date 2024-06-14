45a3eff1330119b59f8422b25c120aff24bc10e6
Sugin Elankavi Rajendran <57343300+Sugin-Elankavi-Rajendran@users.noreply.github.com>
2024-06-14 13:42:10 +0530

    Imported the necessary libraries

---
+++ Singapore.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import json
+import requests
+import pandas as pd
+from geopy.distance import geodesic
+import statistics
+import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
