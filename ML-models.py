6df59d073ce2ac8402d0bbe9294dc2277f54aaac
Sugin Elankavi Rajendran <57343300+Sugin-Elankavi-Rajendran@users.noreply.github.com>
2024-06-14 12:54:18 +0530

    Importing and reading data

---
+++ ML-models.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import statistics
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
+
+
+df = pd.read_csv('combined.csv')
+print(df)
+
