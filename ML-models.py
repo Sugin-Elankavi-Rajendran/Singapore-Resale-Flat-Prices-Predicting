9b81e1e71f5d508c52a7557bbe1b4e090bf5d757
Sugin Elankavi Rajendran <57343300+Sugin-Elankavi-Rajendran@users.noreply.github.com>
2024-06-14 13:00:56 +0530

    finding skewness in data

--- ML-models.py
+++ ML-models.py
@@ -35,3 +35,28 @@ print(scope_df)
 scope_df = scope_df.drop_duplicates()
 print(scope_df)
 
+col = ['cbd_dist','min_dist_mrt','floor_area_sqm','lease_remain_years','storey_median','resale_price']
+
+for i in col:
+    plt.figure(figsize=(8, 6))  
+    sns.boxplot(data=df, x=i)
+    plt.title(f'Boxplot of {i}')
+    plt.xlabel(i)
+    plt.show()
+
+df1 = scope_df
+
+df1['floor_area_sqm'] = np.log(df1['floor_area_sqm'])
+sns.boxplot(x='floor_area_sqm', data=df1)
+plt.show()
+
+df1['storey_median'] = np.log(df1['storey_median'])
+sns.boxplot(x='storey_median', data=df1)
+plt.show()
+
+df1['resale_price'] = np.log(df1['resale_price'])
+sns.boxplot(x='resale_price', data=df1)
+plt.show()
+
+print(df1.dtypes)
+
